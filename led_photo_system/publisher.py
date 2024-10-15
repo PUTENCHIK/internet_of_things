9686cecb92cab5f1dcc8939fb584678b6541dada
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2024-10-15 10:19:47 +0800

    Completed script of publisher in led_photo_system

--- led_photo_system/publisher.py
+++ led_photo_system/publisher.py
@@ -1,7 +1,11 @@
 import serial
 import paho.mqtt.client as mqtt_client
-import time
-from common import get_id, get_topic
+from common import (
+    get_id,
+    get_topic,
+    send_command,
+    config
+)
 
 
 broker = "broker.emqx.io"
@@ -13,18 +17,25 @@ client = mqtt_client.Client(
    pub_id
 )
 
-print("Connecting to broker", broker)
+port_photo = "COM4"
+responses = config()['responses']
+command = responses['photo']['command']
+command_value = responses['photo']['value']
+connection_photo = serial.Serial(port_photo, timeout=1)
+
+print("Connecting to broker:", broker)
 print(client.connect(broker))
 client.loop_start()
-print("Publishing")
+print("Publishing starts")
 
 try:
     while True:
-        state = "Danny Shelby: "
-        inp = input()
-        state += inp
-        print(f"State is {state}")
-        client.publish(f"lab/{pub_id}/led/state", state)
+        photo_val_resp: int = send_command(command,
+                                           command_value,
+                                           connection_photo)
+
+        # print(f"Value: {photo_val_resp}")
+        client.publish(topic, photo_val_resp)
 except KeyboardInterrupt:
     pass
 finally:
