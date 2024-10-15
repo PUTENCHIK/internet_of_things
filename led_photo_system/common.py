bba33008d08cd5b1365f248919516a9288202f90
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2024-10-15 10:01:24 +0800

    Modified led_photo_system/common.py

--- led_photo_system/common.py
+++ led_photo_system/common.py
@@ -1,4 +1,5 @@
 import hashlib
+import serial
 from uuid import getnode as get_mac
 
 def get_broker_id():
@@ -11,6 +12,7 @@ def our_hash(string):
 
     return h.hexdigest()[:20]
 
+
 def get_id():
     mac = get_mac()
     id = our_hash(mac)
@@ -21,3 +23,28 @@ def get_id():
 def get_topic():
     keyword = "DANNY IS AMOGUS"
     return our_hash(keyword)
+
+
+def send_command(cmd: str,
+                 response_len: int,
+                 connection: serial.Serial
+    ) -> int:
+    str_resp: int = 0
+    connection.write(cmd.encode())
+    if response_len > 0:
+        resp: bytes = connection.read(response_len)
+        str_resp = int.from_bytes(resp, 'little')
+
+    return str_resp
+
+
+def config():
+    obj = {
+        "responses": {'d': 7,
+                      'u': 6,
+                      'p': 1,},
+        "LIMIT_1023": 500,
+    }
+    obj["LIMIT_255"] = obj['LIMIT_1023'] // 4
+
+    return obj
