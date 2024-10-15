752ff10b363b9b3c8e2cb11d92f5998b4aef98b6
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2024-10-15 09:36:38 +0800

    Removed led_photo_distributed_serial/subscriber.py and led_photo_distributed_serial/publisher.py to own directory

---
+++ led_photo_system/subscriber.py
@@ -0,0 +1,33 @@
+import serial
+import paho.mqtt.client as mqtt_client
+import time
+import random
+
+broker = "broker.emqx.io"
+
+# client = mqtt_client.Client('isu100123234235')
+# FOR new version change ABOVE line to
+client = mqtt_client.Client(
+   mqtt_client.CallbackAPIVersion.VERSION1,
+   'DANNY_DOT_COM_1001230034'
+)
+
+pub_id = "bc27bab771"
+
+print("Connecting to broker", broker)
+print(client.connect(broker))
+client.loop_start()
+print("Publishing")
+
+try:
+    while True:
+        state = "Danny Shelby: "
+        inp = input()
+        state += inp
+        print(f"State is {state}")
+        client.publish(f"lab/{pub_id}/led/state", state)
+except KeyboardInterrupt:
+    pass
+finally:
+    client.disconnect()
+    client.loop_stop()
