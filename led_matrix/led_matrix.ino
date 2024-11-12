3655e6bf4c2b459a052c3eafbf7edfdb60662d19
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2024-11-12 09:54:33 +0800

    Task LedMatrix Audience moved to led_matrix_audit/. Added dir for task LedMatrix

--- led_matrix/led_matrix.ino
+++ led_matrix/led_matrix.ino
@@ -1,77 +1,9 @@
-#define D3 3
-#define D4 4
-#define D5 5
-#define D6 6
-#define DURATION 1000
-
-
-int pins[] = {D6, D5, D4, D3};
-
-struct MyDict {
-    String name;
-    byte value;
-};
-
-
-const int statesLength = 2; 
-MyDict states[] = {
-    {"1", B10010000},
-    {"2", B10100000}
-};
-
-
-byte getValue(String key) {
-    for (int i = 0; i < statesLength; i++) {
-        if (states[0].name == key) {
-            return states[0].value;
-        }
-    }
-
-    return 0;
-}
-
-
-
-void setState(byte state) {
-    for (int i = 0; i < 4; i++) {
-        digitalWrite(pins[i], state & 0b00000001);
-        state >>= 1;
-    }
-    delay(1);
-    for (int i = 4; i < 8; i++) {
-        digitalWrite(pins[i], state & 0b00000001);
-        state >>= 1;
-    }
-    delay(1);
-}
-
-
-bool isDiagonal = false;
-long int start = millis();
-
-
 void setup() {
   // put your setup code here, to run once:
-    Serial.begin(9600);
-    pinMode(D3, OUTPUT);
-    pinMode(D4, OUTPUT);
-    pinMode(D5, OUTPUT);
-    pinMode(D6, OUTPUT);
+
 }
 
 void loop() {
-    if (Serial.available()) {
-        String cmd = Serial.readString();
-        byte state = getValue(cmd);
-        Serial.println(cmd);
-        Serial.println(state);
+  // put your main code here, to run repeatedly:
 
-        setState(state);
-    }
-
-    // if (millis() >= start + DURATION) {
-    //     start = millis();
-    //     isDiagonal = isDiagonal ? false : true;
-    // }
 }
-
