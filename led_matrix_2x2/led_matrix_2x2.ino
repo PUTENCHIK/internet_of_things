d2f2ae79a36d00c2a568bbbd1e2cfeeff8bc0994
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2024-11-25 16:09:43 +0800

    Completed task LedMatrix

--- led_matrix_2x2/led_matrix_2x2.ino
+++ led_matrix_2x2/led_matrix_2x2.ino
@@ -2,9 +2,9 @@
 #define D4 4
 #define D5 5
 #define D6 6
-#define DURATION 1000
 
 
+const int DURATION = 1000;
 int pins[4] = {D6, D5, D4, D3};
 
 struct MyDict {
@@ -12,24 +12,39 @@ struct MyDict {
     byte value;
 };
 
-const int statesLength = 2; 
+const int statesLength = 16; 
 
 MyDict states[] = {
+    {"off", B00000000},
+    {"on", B11000000},
     {"1", B10010000},
-    {"2", B10100000}
+    {"2", B10100000},
+    {"3", B01010000},
+    {"4", B01100000},
+    {"12", B10000000},
+    {"24", B11100000},
+    {"34", B01000000},
+    {"13", B11010000},
+    {"14", B10010110},
+    {"23", B10100101},
+    {"124", B10000110},
+    {"234", B10100100},
+    {"123", B10000101},
+    {"134", B10010100},
 };
 
-bool isDiagonal = false;
-long int start = millis();
+bool isSet = false;
+byte fullCurrentState;
+byte currentState;
 
 
 byte getValue(String key) {
     for (int i = 0; i < statesLength; i++) {
-        if (states[0].name == key) {
-            return states[0].value;
+        if (states[i].name == key) {
+            return states[i].value;
         }
     }
-    return 0;
+    return -1;
 }
 
 
@@ -39,11 +54,13 @@ void setState(byte state) {
         state >>= 1;
     }
     delay(1);
-    for (int i = 4; i < 8; i++) {
-        digitalWrite(pins[i], state & 0b00000001);
-        state >>= 1;
+}
+
+
+void reset() {
+    for (int i = 0; i < 4; i++) {
+        digitalWrite(pins[i], LOW);
     }
-    delay(1);
 }
 
 
@@ -55,15 +72,28 @@ void setup() {
     pinMode(D6, OUTPUT);
 }
 void loop() {
+    if (isSet) {
+        if (currentState == 0) {
+            currentState = fullCurrentState;
+        }
+        setState(currentState);
+        currentState >>= 4;
+    }
+  
     if (Serial.available()) {
+        reset();
         String cmd = Serial.readString();
         byte state = getValue(cmd);
-        Serial.println(cmd);
-        Serial.println(state);
-        setState(state);
+        
+
+        if (state != byte(-1) && (!isSet || state != fullCurrentState)) {
+            Serial.print(cmd + "\t");
+            Serial.println(state);
+            fullCurrentState = state;
+            currentState = state;
+            isSet = fullCurrentState != 0;
+        } else {
+            Serial.println("Bad command: " + cmd);
+        }
     }
-    // if (millis() >= start + DURATION) {
-    //     start = millis();
-    //     isDiagonal = isDiagonal ? false : true;
-    // }
 }
