db5eb011dc505b0839e95c4cfc99a033a6123ee1
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2025-04-09 11:38:20 +0800

    Added slave script for spi_example

--- spi_example/slave/slave.ino
+++ spi_example/slave/slave.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#include<SPI.h>
 
+#define RES A0
+
+volatile boolean received;
+volatile byte slaveReceived, slaveSend;
+const int DELAY = 100;
+long int timer;
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(MISO, OUTPUT);
+    SPCR |= _BV(SPE);                       // SPI into Slave Mode
+    received = false;
+    SPI.attachInterrupt();                  // SPI interrupt ON
+    timer = millis();
+}
+
+ISR (SPI_STC_vect) {
+  slaveReceived = SPDR;
+  received = true;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    if (millis() > timer + DELAY) {
+        Serial.print("Recevied value: ");
+        Serial.println(slaveReceived);
+        if (received && slaveReceived) {
+            int value = analogRead(RES);
+            Serial.print("PHOTO value: ");
+            Serial.println(value);
+            SPDR = value;
+            received = false;
+        }
+        timer = millis();
+    }
 }
